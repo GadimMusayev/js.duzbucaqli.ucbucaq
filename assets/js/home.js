e5005c2c583ca45aa9d3d953171c11d1500193cd
GadimMusayev <musayevqdim@gmail.com>
2024-08-27 08:28:39 +0200

    first commit

---
+++ assets/js/home.js
@@ -0,0 +1,31 @@
+for(var i = 1; i<=1; i++){
+    console.log("*************" )
+  
+   for(var i = 1; i<=1; i++){
+    console.log("*")
+   }
+   for(var i = 1; i<=1; i++){
+    console.log("*")
+   }
+   for(var i = 1; i<=1; i++){
+    console.log("*")
+   }
+   for(var i = 1; i<=1; i++){
+    console.log("*")
+   }
+   for(var i = 1; i<=1; i++){
+    console.log("*")
+   }
+   for(var i = 1; i<=1; i++){
+    console.log("*")
+   }
+   for(var i = 1; i<=1; i++){
+    console.log("*")
+   }
+   for(var i = 1; i<=1; i++){
+    console.log("*")
+   }
+   for(var i = 1; i<=1; i++){
+    console.log("**************" )}
+
+}
